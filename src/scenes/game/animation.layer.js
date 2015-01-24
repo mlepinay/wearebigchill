de79ded2181d4f4aa2a00c366e51ea507bba1b55
mlepinay <mlepinay@mlepinay-GP60-2OD.(none)>
2015-01-24 01:26:21 +0000

    Physics ready

--- src/scenes/game/animation.layer.js
+++ src/scenes/game/animation.layer.js
@@ -1,9 +1,10 @@
 var AnimationLayer = cc.Layer.extend({
     sprite:null,
-    ctor:function () {
+    ctor:function (space) {
         //////////////////////////////
         // 1. super init first
         this._super();
+        this.space = space;
         this.init();
     },
 
@@ -11,7 +12,24 @@ var AnimationLayer = cc.Layer.extend({
         var whale = new Whale();
         this.addChild(whale.sprite, 0);
 
-        console.log(whale);
+        //1. create PhysicsSprite with a sprite frame name
+        this.sprite = new cc.PhysicsSprite(res.docker_whale);
+        var contentSize = this.sprite.getContentSize();
+        // 2. init the runner physic body
+        this.body = new cp.Body(1, cp.momentForBox(1, contentSize.width, contentSize.height));
+        //3. set the position of the runner
+        this.body.p = cc.p(g_runnerStartX, g_groundHeight + contentSize.height);
+        //4. apply impulse to the body
+        // this.body.applyImpulse(cp.v(150, 0), cp.v(0, 0));//run speed
+        //5. add the created body to space
+        this.space.addBody(this.body);
+        //6. create the shape for the body
+        this.shape = new cp.BoxShape(this.body, contentSize.width - 14, contentSize.height);
+        //7. add shape to space
+        this.space.addShape(this.shape);
+        //8. set body to the physic sprite
+        this.sprite.setBody(this.body);
+
         return true;
     }
 });
--- src/scenes/game/game.scene.js
+++ src/scenes/game/game.scene.js
@@ -1,8 +1,34 @@
 var GameScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new AnimationLayer();
+        this.initPhysics();
+
+        var layer = new AnimationLayer(this.space);
         this.addChild(layer);
+
+        this.scheduleUpdate();
+    },
+
+    space:null,
+
+        // init space of chipmunk
+    initPhysics:function() {
+        //1. new space object 
+        this.space = new cp.Space();
+        //2. setup the  Gravity
+        this.space.gravity = cp.v(0, -350);
+
+        // 3. set up Walls
+        var wallBottom = new cp.SegmentShape(this.space.staticBody,
+            cp.v(0, g_groundHeight),// start point
+            cp.v(4294967295, g_groundHeight),// MAX INT:4294967295
+            0);// thickness of wall
+        this.space.addStaticShape(wallBottom);
+    },
+
+    update:function (dt) {
+        // chipmunk step
+        this.space.step(dt);
     }
 });
 
