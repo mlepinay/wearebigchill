ead0764def1f2d6ee4f10110f9abf46a8a38a174
mlepinay <mlepinay@mlepinay-GP60-2OD.(none)>
2015-01-25 00:45:54 +0000

    Missing files

---
+++ src/scenes/game/status.layer.js
@@ -0,0 +1,42 @@
+var StatusLayer = cc.Layer.extend({
+    labelScore:null,
+    labelCombo:null,
+    score:0,
+    combo:0,
+    lives:5,
+
+    ctor:function () {
+        this._super();
+        this.init();
+    },
+
+    init:function () {
+        this._super();
+
+        var winsize = cc.director.getWinSize();
+
+        this.labelScore = new cc.LabelTTF("Score: "+this.score, "Helvetica", 20);
+        this.labelScore.setColor(cc.color(0,0,0));//black color
+        this.labelScore.setPosition(cc.p(70, winsize.height - 20));
+        this.addChild(this.labelScore);
+
+        this.labelCombo = new cc.LabelTTF("Combo: "+this.combo, "Helvetica", 20);
+        this.labelCombo.setPosition(cc.p(winsize.width - 70, winsize.height - 20));
+        this.addChild(this.labelCombo);
+    },
+
+    updateScore:function (num) {
+        this.score = num;
+        this.labelScore.setString("Score: "+this.score);
+    },
+
+    updateCombo:function (num) {
+        this.combo = num;
+        this.labelCombo.setString("Combo: "+this.combo);
+    },
+
+    updateLives:function (num) {
+        this.lives = num;
+    }
+
+});
