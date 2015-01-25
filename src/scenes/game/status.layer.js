e8ad2afa1eb795fc7712e1b885d25563092b294b
Alex Santos <alex@yieldify.com>
2015-01-25 01:45:44 +0000

    Choose next container

--- src/scenes/game/status.layer.js
+++ src/scenes/game/status.layer.js
@@ -4,6 +4,7 @@ var StatusLayer = cc.Layer.extend({
     score:0,
     combo:0,
     lives:5,
+    selected: 0,
 
     ctor:function () {
         this._super();
@@ -14,6 +15,26 @@ var StatusLayer = cc.Layer.extend({
         this._super();
 
         var winsize = cc.director.getWinSize();
+        var type, sprite;
+
+        this.containers = [];
+        this.containerNames = [];
+        for (var key in ContainerType) {
+            this.containerNames.push(key);
+            type = ContainerType[key];
+            sprite = new cc.Sprite(type.sprite);
+            sprite.color = type.color || cc.color(255,255,255,255);
+            sprite.scale = type.scale || SCALE;
+            this.containers.push(sprite);
+            this.addChild(sprite, 1);
+        }
+
+        this.selector = cc.Sprite.create();
+        this.selector.color = cc.color(0,150,0,255);
+        this.selector.setOpacity(150);
+        this.addChild(this.selector);
+
+        this.drawContainerMenu();
 
         this.labelScore = new cc.LabelTTF("Score: "+this.score, "Helvetica", 20);
         this.labelScore.setColor(cc.color(0,0,0));//black color
@@ -23,6 +44,39 @@ var StatusLayer = cc.Layer.extend({
         this.labelCombo = new cc.LabelTTF("Combo: "+this.combo, "Helvetica", 20);
         this.labelCombo.setPosition(cc.p(winsize.width - 70, winsize.height - 20));
         this.addChild(this.labelCombo);
+
+        var layer = this;
+        var listener = cc.EventListener.create({
+
+            event: cc.EventListener.KEYBOARD,
+
+            onKeyReleased: function (keyCode, event) {
+                if (keyCode === 32) {
+                    layer.selected = (layer.selected + 1) % layer.containers.length;
+                    layer.drawContainerMenu();
+                }
+            }
+        });
+        cc.eventManager.addListener(listener, 1);
+    },
+
+    drawContainerMenu: function() {
+        var sprite, winsize = cc.director.getWinSize();
+        for (var i = 0; i < this.containers.length; i++) {
+            sprite = this.containers[i];
+            sprite.setPosition(
+                cc.p(winsize.width - sprite.width * sprite.scale - 10,
+                    winsize.height - 100 - (sprite.height*sprite.scale+15) * i)
+            )
+            if (this.selected === i) {
+                this.selectedContainer = this.containerNames[i];
+                this.selector.setTextureRect(cc.rect(0, 0, sprite.width*sprite.scale+10, sprite.height*sprite.scale+10));
+                this.selector.setPosition(
+                    cc.p(winsize.width - sprite.width * sprite.scale - 10,
+                        winsize.height - 100 - (sprite.height*sprite.scale+15) * i)
+                )
+            }
+        }
     },
 
     updateScore:function (num) {
